042d079dfa1ec122e1023581207569935c8d07a5
Berfinesme <berfinesme7@gmail.com>
2025-02-09 20:27:50 +0300

    Add files via upload

---
+++ 250214/ornek1.py
@@ -0,0 +1,3 @@
+haftanin_günleri= ["pazartesi","sali","çarsamba","persembe","cuma"]
+print(haftanin_günleri[4])
+#çikti,cuma 
---
+++ 250214/ornek2.py
@@ -0,0 +1,15 @@
+ders= "kodlama"
+print(ders(0))
+#çikti,k
+print(ders[1])
+#çikti,o
+print(ders[2])
+#çikti,d
+print(ders[3])
+#çikti,l
+print(ders[4])
+#çikti,a
+print(ders[5])
+#çikti,m
+print(ders[6])
+#çikti,a
