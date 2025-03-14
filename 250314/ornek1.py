816a9d9ba4b0e694e87e87662e91ad3d0255aa98
Berfinesme <berfinesme7@gmail.com>
2025-03-14 12:02:55 +0300

    Add files via upload

---
+++ 250314/ornek1.py
@@ -0,0 +1,7 @@
+miktar=int(input("lütfen kg girin"))
+if miktar <=10:
+    ucret=miktar*10
+    print("ödeme :",ucret)
+else :
+    ucret=100 + (miktar-10)*7.5
+    print("odeme:",ucret)
---
+++ 250314/ornek2.py
@@ -0,0 +1,6 @@
+kad=input("kullanici adinizi girin :")
+ksifre=input("kullanici sifresini girin:")
+if kad== "Türkiye" and ksifre =="1923":
+    print("giris basarili")
+else:
+    print("kullanici adi yada sifre yanlis")
